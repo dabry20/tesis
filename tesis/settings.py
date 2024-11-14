3debab4b79c2341a39fb415f465f014a33cd658d
Dav-007 <densymp@gmail.com>
2024-11-14 14:13:44 -0500

    gitingnoree

--- tesis/settings.py
+++ tesis/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 
 import dj_database_url
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -22,12 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rbriitvkv4gln9dj%22644wmjgcw2(x09-9j7j0pg^%rl_1e5w'
+# SECRET_KEY = 'django-insecure-rbriitvkv4gln9dj%22644wmjgcw2(x09-9j7j0pg^%rl_1e5w'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG","False").lower()=="True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
